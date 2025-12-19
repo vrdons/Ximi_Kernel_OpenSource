d37ee1ababc66f1385fd58917aba228b15b8e7c3
vrdons <vrdons@proton.me>
2025-12-19 17:27:55 +0300

    teei: Import xiaomi changes

--- drivers/misc/mediatek/teei/300/tz_driver/tz_log.c
+++ drivers/misc/mediatek/teei/300/tz_driver/tz_log.c
@@ -220,9 +220,9 @@ int teei_log_fn(void *work)
 		if (retVal != 0)
 			continue;
 #ifdef CONFIG_MICROTRUST_TZ_LOG
-		//spin_lock_irqsave(&s->lock, flags);
+		spin_lock_irqsave(&s->lock, flags);
 		tz_driver_dump_logs(s);
-		//spin_unlock_irqrestore(&s->lock, flags);
+		spin_unlock_irqrestore(&s->lock, flags);
 #endif
 	}
 
@@ -435,4 +435,3 @@ int tz_log_remove(struct platform_device *pdev)
 
 	return 0;
 }
-
