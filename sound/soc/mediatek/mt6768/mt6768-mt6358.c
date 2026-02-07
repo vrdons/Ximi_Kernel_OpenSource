e41ef514305e2862f9280fdf379f48a53e3a7a1d
vrdons <vrdons@proton.me>
2026-02-07 11:01:38 +0300

    import selene change

--- sound/soc/mediatek/mt6768/mt6768-mt6358.c
+++ sound/soc/mediatek/mt6768/mt6768-mt6358.c
@@ -70,6 +70,24 @@ static int mt6768_spk_i2s_in_type_get(struct snd_kcontrol *kcontrol,
 	return 0;
 }
 
+#ifdef CONFIG_SND_SOC_AW87559
+enum {
+	AW87XXX_OFF_MODE = 0,
+	AW87XXX_MUSIC_MODE = 1,
+	AW87XXX_VOICE_MODE = 2,
+	AW87XXX_FM_MODE = 3,
+	AW87XXX_RCV_MODE = 4,
+	AW87XXX_MODE_MAX = 5,
+};
+enum {
+	AW87XXX_LEFT_CHANNEL = 0,
+	AW87XXX_RIRHT_CHANNEL = 1,
+};
+
+extern unsigned char aw87xxx_show_current_mode(int32_t channel);
+extern int aw87xxx_audio_scene_load(uint8_t mode, int32_t channel);
+#endif
+
 #ifdef CONFIG_SND_SOC_AW87519
 extern unsigned char aw87519_audio_kspk(void);
 extern unsigned char aw87519_audio_drcv(void);
@@ -92,11 +110,17 @@ static int mt6768_mt6358_spk_amp_event(struct snd_soc_dapm_widget *w,
 #ifdef CONFIG_SND_SOC_AW87519
 		aw87519_audio_kspk();
 #endif
+#ifdef CONFIG_SND_SOC_AW87559
+	aw87xxx_audio_scene_load(AW87XXX_MUSIC_MODE, AW87XXX_LEFT_CHANNEL);
+#endif
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		/* spk amp off control */
 #ifdef CONFIG_SND_SOC_AW87519
 		aw87519_audio_off();
+#endif
+#ifdef CONFIG_SND_SOC_AW87559
+	aw87xxx_audio_scene_load(AW87XXX_OFF_MODE, AW87XXX_LEFT_CHANNEL);
 #endif
 		break;
 	default:
